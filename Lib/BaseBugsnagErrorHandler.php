221fff8ae39f6b862b80cf0b506ca886641b314c
Shigehiro Ishita <shigehiro.ishita@howtelevision.jp>
2016-05-02 12:22:27 +0900

    add BugsnagUtils

--- Lib/BaseBugsnagErrorHandler.php
+++ Lib/BaseBugsnagErrorHandler.php
@@ -21,6 +21,7 @@
  * limitations under the License.
  */
 
+require_once dirname(__FILE__).DS.'BugsnagUtils.php';
 /**
  * @trait BaseBugsnagErrorHandler`
  */
@@ -33,19 +34,7 @@ trait BaseBugsnagErrorHandler
     {
         static $bugsnag = null;
         if (null === $bugsnag) {
-            $bugsnag = new \Bugsnag_Client(Configure::read('BugsnagCakephp.apikey'));
-            $bugsnag->setBatchSending(false);
-            $bugsnag->setNotifier(array(
-                'name'    => 'Bugsnag CakePHP',
-                'version' => '0.1.0',
-                'url'     => 'https://github.com/Label305/bugsnag-cakephp'
-            ));
-            $bugsnag->setReleaseStage(Configure::read('BugsnagCakephp.releaseStage'));
-            $bugsnag->setType("CakePHP");
-            $filters = Configure::read('BugsnagCakephp.filters');
-            if (!empty($filters)) {
-                $bugsnag->setFilters($filters);
-            }
+            $bugsnag = BugsnagUtils::createClient();
         }
 
         return $bugsnag;
